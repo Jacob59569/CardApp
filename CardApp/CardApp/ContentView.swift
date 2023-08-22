84da00720aa1b449aa2dee90b8b396310233fbca
Jacob <110312077+Jacob59569@users.noreply.github.com>
2023-08-23 00:15:38 +0300

    folder

--- CardApp/CardApp/ContentView.swift
+++ CardApp/CardApp/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "folder.fill")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, jacob, what a great day!")
+                
         }
         .padding()
     }
