fd22cc2aa36d56ee9e7fb2f241f0fb97f1a9bb98
Jacob <110312077+Jacob59569@users.noreply.github.com>
2023-08-22 17:53:23 +0300

    This is app

---
+++ CardApp/CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by Jacob on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
